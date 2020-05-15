1412b3c186d54b99e7c1babb302e576154ce06cf
rhryhoryk <eh480.35@gmail.com>
2020-05-15 12:50:47 +0300

    fix: new-add-block template

---
+++ src/components/new-add-block.js
@@ -0,0 +1,32 @@
+import Component from './component.js';
+
+const createNewAddBlockTemplate = (addingType) => {
+  return (
+    `<div class="group group--new">
+      <input type="text" name="new" id="newItem" class="input" placeholder="enter new ${addingType}..." autocomplete="off">
+      <div class="group group--buttons">
+        <button class="button button--add" data-value="${addingType}">add</button>
+        <button class="button button--service button--cancel" data-value="${addingType}">X</button>
+      </div>
+    </div>`
+  )
+}
+
+export default class NewAddBlock extends Component {
+  constructor(addingType) {
+    super();
+    this._addingType = addingType;
+  } 
+
+  getTemplate() {
+    return createNewAddBlockTemplate(this._addingType);
+  }
+
+  onAddbuttonClick(handler) {
+    this.getElement().querySelector(`.button--add`).addEventListener(`click`, handler);
+  } 
+
+  onCancelButoonClick(handler) {
+    this.getElement().querySelector(`.button--cancel`).addEventListener(`click`, handler);
+  } 
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import NewListButton from './components/new-list-buttom.js';
-import AddNewBlock from './components/add-new-block.js'
+import NewAddBlock from './components/new-add-block';
 import TaskCard from './components/task-card.js';
 import TaskList from './components/task-list.js';
 
